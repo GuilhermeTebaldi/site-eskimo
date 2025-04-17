ed811a814b09ea1f299a0777878c1b3e6918593b
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-04-17 12:48:21 -0300

    Corrigido crossOrigin para anonymous

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,17 @@ export default function App() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          crossOrigin="anonymous" // ✅ Corrigido aqui!
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
         <style>{`
-          body {
-            font-family: 'Inter', sans-serif;
-          }
-        `}</style>
+    body {
+      font-family: 'Inter', sans-serif;
+    }
+  `}</style>
       </Helmet>
 
       <div className="bg-white text-gray-800">
